b88106d438a596fdabced7b1444f7382dea51269
Yerasil0405 <saparbeke04@mail.ru>
2025-05-12 12:29:27 +0500

    Your commit message here

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from boards.models import Board
+
+admin.site.register(Board)
+from django.contrib import admin
+
 # Register your models here.
--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from cards.models import Card
+
+admin.site.register(Card)
